1c01740c199753fa58a59621651994b511b89e35
Collins Mwiti <collotush@gmail.com>
2017-05-05 17:52:22 +0300

    final push

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,5 +3,6 @@ import org.sql2o.*;
 
 // to link with the database
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do", "collins", "password");
+  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do", "collins", "password");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-227-237-223.compute-1.amazonaws.com:5432/d5iq6mbp37iuee", "rsdbegjlwqmmlr", "50f18f4d41074e2a53abc98c2e90cab3b3bfe28b19d32c324b4ad7f22a9a2f92");
 }
